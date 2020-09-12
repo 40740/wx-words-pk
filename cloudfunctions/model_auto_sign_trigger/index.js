850eb016cca9c110d3c3ed60c4d0e07595d1d1ce
arleyGuoLei <34805850@qq.com>
2020-09-12 13:51:15 +0800

    感谢支持 ~ 继续努力

--- cloudfunctions/model_auto_sign_trigger/index.js
+++ cloudfunctions/model_auto_sign_trigger/index.js
@@ -8,6 +8,8 @@ const db = cloud.database()
 const userModel = db.collection('pk_user')
 const _ = db.command
 
+// 自动签到的几个用户，用于带动排行榜单，可以修改下方数组为自己的openid，然后设置触发器
+
 exports.main = async () => {
   const openids = ['ovro243wibR0ajKtzO15NVefQxV4', 'ovro24_M0Wj2iU9-ShcbW2avcr0k', 'ovro242zgOano-3cPdXrQHraPRug', 'ovro240jsZm_GyrHNh7luqa-qqys']
   return userModel.where({
